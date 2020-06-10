c48d605c5838567674fbd0175407fb20fa6a5ccf
wjphe1 <wjphe1@student.monash.edu>
2020-06-10 10:47:14 +0800

    removed redundant files

--- exam-scrap-env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\lawli_000\desktop\image-scraper\exam-scrap-env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
