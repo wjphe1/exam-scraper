2bcb9dc33f0173b96349b7a10cd23b8b2f598bae
wjphe1 <wjphe1@student.monash.edu>
2020-06-14 19:56:56 +0800

    fixed timeout issue

--- scrape.py
+++ scrape.py
@@ -2,6 +2,8 @@ from bs4 import BeautifulSoup
 import urllib.request
 import requests
 from requests import Session
+import time
+
 
 from requests.packages.urllib3 import add_stderr_logger
 import urllib
@@ -14,7 +16,7 @@ random.seed(datetime.datetime.now())
 
 session = requests.Session()
 per_session = session.post("https://www.exam-mate.com/reguser/checklogin", 
-data={'email':'lawliet5145@gmail.com', 'password':'kAmehAmehA1!'})
+data={'email':'lawliet5145@gmail.com', 'password':'kAmehAmehA1!'},timeout=None)
 
 
 chs=[534,535,536,537,538,539]
@@ -39,41 +41,45 @@ for x in chs:
     CountStr = 0
     counting = 0
     count = 0
-    while (count < len(arrUrl)):
-        """ response = urllib.request.urlopen(arrUrl[count]) """
-        content = BeautifulSoup(session.get(arrUrl[count]).content , "html.parser") # Use the session to get url
-        with open('try.html', "w", encoding="utf-8") as f:
-            f.write(str(content.find_all('a', class_="form-control")))
-        if (content.find_all('a', class_="qabtn") == None):
-            break
-        else:
-            for anc in content.find_all('a', class_="qabtn"):
-                splitting = anc['onclick'].split(",")[1]
-                spli = splitting.translate({ord("'"): None})
-                if len(spli) > 2:
-                    adding = "https://www.exam-mate.com"+spli
-                    spli = adding.replace(" ","")
-                arr.append(spli)
 
-            for tit in content.find_all("div" , class_="question"):
-                for title in tit.find_all("div"):
-                    AlasOfLast.append(title.string)
+    try:
+        while (count < len(arrUrl)):
+            """ response = urllib.request.urlopen(arrUrl[count]) """
+            content = BeautifulSoup(session.get(arrUrl[count]).content , "html.parser") # Use the session to get url
+            with open('try.html', "w", encoding="utf-8") as f:
+                f.write(str(content.find_all('a', class_="form-control")))
+            if (content.find_all('a', class_="qabtn") == None):
+                break
+            else:
+                for anc in content.find_all('a', class_="qabtn"):
+                    splitting = anc['onclick'].split(",")[1]
+                    spli = splitting.translate({ord("'"): None})
+                    if len(spli) > 2:
+                        adding = "https://www.exam-mate.com"+spli
+                        spli = adding.replace(" ","")
+                    arr.append(spli)
 
-            while (CountStr < len(AlasOfLast)):
-                if (AlasOfLast[CountStr] == None):
-                    del AlasOfLast[CountStr]
-                CountStr += 1
+                for tit in content.find_all("div" , class_="question"):
+                    for title in tit.find_all("div"):
+                        AlasOfLast.append(title.string)
 
-            while (counting < len(AlasOfLast)):
-                if (counting % 2 == 0):
-                    lenOfArr.append(AlasOfLast[counting])
-                counting += 1
-                    
-            count += 1
+                while (CountStr < len(AlasOfLast)):
+                    if (AlasOfLast[CountStr] == None):
+                        del AlasOfLast[CountStr]
+                    CountStr += 1
 
+                while (counting < len(AlasOfLast)):
+                    if (counting % 2 == 0):
+                        lenOfArr.append(AlasOfLast[counting])
+                    counting += 1
+                        
+                count += 1
 
-    for n in range(len(lenOfArr)) :
-        NewArr.append(lenOfArr[n].strip())
+        for n in range(len(lenOfArr)) :
+            NewArr.append(lenOfArr[n].strip())
+
+    except:
+        print("timeout or connection reset by peer")
 
     f = open("hello.py", "w")
     f.write("NewArr = {}\n".format(NewArr))
@@ -97,19 +103,33 @@ for x in chs:
 
     quest = []
     answe = []
-    for i in range(len(ques)):
-        print('{} in {}' .format(i+1,len(ques)))
-        for k in range(1,11):
-            if len(ques[i]) > 25:
+    try:
+        for i in range(len(ques)):
+            print('{} in {}'.format(i+1,len(ques)))
+            for k in range(1,11):
                 cont = ques[i].replace('_1.png',"_{k}.png".format(k=k)) if (ques[i].endswith("png")) else ques[i].replace('_1.jpg',"_{k}.jpg".format(k=k)) if (ques[i].endswith("jpg")) else ques[i].replace('_1.jpeg',"_{k}.jpeg".format(k=k))
                 if (requests.get(cont).status_code == 200):
                     quest.append(cont)
                     print(cont)
-            if len(answ[i]) > 25:
-                cant = answ[i].replace('_1.png',"_{k}.png".format(k=k)) if (answ[i].endswith("png")) else answ[i].replace('_1.jpg',"_{k}.jpg".format(k=k)) if (answ[i].endswith("jpg")) else answ[i].replace('_1.jpeg',"_{k}.jpeg".format(k=k))
-                if (requests.get(cant).status_code == 200):
-                    answe.append(cant)
-                    print(cant)
+                cant = answ[i].replace('_1.png',"_{k}.png".format(k=k)) if (answ[i].endswith("png")) else answ[i].replace('_1.jpg',"_{k}.jpg".format(k=k)) if (answ[i].endswith("jpg")) else answ[i].replace('_1.jpeg',"_{k}.jpeg".format(k=k)) if (answ[i].endswith('jpeg')) else answ[i]
+                try:
+                    if (answ[i].endswith("png") or answ[i].endswith("jpg") or answ[i].endswith("jpeg")):
+                        if (requests.get(cant).status_code == 200):
+                            answe.append(cant)
+                            print(cant)
+                        else:
+                            pass
+                            break
+                    else:
+                        answe.append(cant)
+                        print(cant)
+                        break
+                except:
+                    print("not image error")
+
+            time.sleep(3)
+    except:
+        print("connection reset by peer or time out")
 
     print('~\n~\n~\nNumber of Questions: {}\nQuestion Images: {}\nAnswer Images: {}'.format(len(NewArr),len(quest),len(answe)))
 
@@ -127,6 +147,7 @@ for x in chs:
     #Question
     titlenum = 0
     for i in range(len(quest)):
+        time.sleep(1)
         if quest[i][-5] != '1':
             filename = NewArr[titlenum-1].replace('/','-') + '_' + quest[i][-5] + '.png'
             filename = filename.replace('(','').replace(')','')
@@ -144,6 +165,7 @@ for x in chs:
     # Answer
     atitnum = 0
     for i in range(len(answe)):
+        time.sleep(1)
         if answe[i][-5] != '1':
             filename = NewArr[atitnum-1].replace('/','-') + '_' + answe[i][-5] + '.png'
             filename = filename.replace('Q','A').replace('(','').replace(')','')
